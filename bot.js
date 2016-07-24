6137250d6404619dd38a208e12dec2b21201e73e
Victor Johnson <vicjohnson1213@gmail.com>
2016-07-24 17:46:15 -0600

    Added all quotes to a single file.

--- bot.js
+++ bot.js
@@ -2,12 +2,12 @@ var cron = require('cron'),
     quotes = require('./quotes/quotes');
     twitter = require('./twitter');
 
-var cron = cron.job('0 0 11 * * *', () => {
+var cron = cron.job('11 11 11 * * *', () => {
     var quote = quotes.next();
 
     if (!quote) {
         cron.stop();
-        return
+        return;
     }
 
     var tweet = `${quote.quote}`;
@@ -16,10 +16,8 @@ var cron = cron.job('0 0 11 * * *', () => {
         tweet += ` - ${quote.author}`;
     }
 
-    console.log(`Tweeting: ${tweet}`);
-    // twitter.tweet(tweet);
+    twitter.tweet(tweet);
 
 }, () => {}, true, 'America/Denver');
 
-console.log('Starting the bot!');
 cron.start();
