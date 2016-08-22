898767fd31aac8eb8686b39e414b8a926070b76a
Victor Johnson <vicjohnson1213@gmail.com>
2016-08-22 15:28:55 -0600

    Fixed small syntax error.

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
-var quotes = require('./quotes');
-    twitter = require('./twitter')
-    argv = require('minimist')(process.argv.slice(2));;
+var quotes = require('./quotes'),
+    twitter = require('./twitter'),
+    argv = require('minimist')(process.argv.slice(2));
 
 var quote = quotes.next(argv);
 
